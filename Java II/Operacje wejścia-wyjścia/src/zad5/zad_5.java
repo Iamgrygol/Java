9a2c6b1813bb3e8e94332426a33448efa8ffb2ab
Mateusz <gryglik.mateusz@gmail.com>
2019-01-27 13:41:24 +0100

    zad_5

--- Java II/Operacje wejścia-wyjścia/src/zad5/zad_5.java
+++ Java II/Operacje wejścia-wyjścia/src/zad5/zad_5.java
@@ -1,24 +1,17 @@
 package zad5;
-/* ================== */
-/* JESZCZE NIE DZIA£A */
-/* ================== */
-import java.io.File;
+
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.RandomAccessFile;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.Scanner;
+
 
 public class zad_5 {
-
 	public static void main(String[] args) throws IOException{
-		URL url = new URL("http://horyzont.eu/");
-		InputStream in = url.openStream();
-		
-		while(url.hasNextLine());
-		
-		
 
-	}
-
+		URL url = new URL("https://pl.wikipedia.org/wiki/Ma%C5%82pa_(znak_pisarski)");
+		BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+		String line;
+		while((line = in.readLine()) != null) if(line.contains("@"))System.out.println(in.readLine() + "\n");
+}
 }
